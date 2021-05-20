303b055ee9e329ff9983f38f8488c1736286a902
yunyoung1819 <yunyoung1819@hanmail.net>
2021-05-21 00:07:37 +0900

    #1 study somoim 회원가입 컨트롤러 구현

---
+++ src/test/java/com/studysomoim/StudysomoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studysomoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudysomoimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
