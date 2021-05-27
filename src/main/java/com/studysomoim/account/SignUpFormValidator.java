e76f0ed58efc7c9a7630f3f7921eac2079214db1
yunyoung1819 <yunyoung1819@hanmail.net>
2021-05-28 01:10:20 +0900

    # 회원 가입 뷰 및 회원 가입 폼 서브밋 검증 구현

---
+++ src/main/java/com/studysomoim/account/SignUpFormValidator.java
@@ -0,0 +1,32 @@
+package com.studysomoim.account;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class SignUpFormValidator implements Validator {
+
+    private AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return aClass.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        // TODO email, nickname
+        SignUpForm signUpForm = (SignUpForm) errors;
+        if (accountRepository.existsByEmail(signUpForm.getEmail())) {
+            errors.rejectValue("email", "invalid.email", new Object[]{signUpForm.getEmail()}, "이미 사용 중인 이메일입니다.");
+        }
+
+        if (accountRepository.existsByNickname(signUpForm.getNickname())) {
+            errors.rejectValue("nickname", "invalid.nickname", new Object[]{signUpForm.getNickname()}, "이미 사용 중인 닉네임입니다.");
+        }
+    }
+}
--- src/test/java/com/studysomoim/account/AccountControllerTest.java
+++ src/test/java/com/studysomoim/account/AccountControllerTest.java
@@ -11,8 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,7 +25,8 @@ class AccountControllerTest {
         mockMvc.perform(get("/sign-up"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(view().name("account/sign-up"));
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(model().attributeExists("signUpForm"));
     }
 
 }
