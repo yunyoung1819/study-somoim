f2abbd650ac364f5047632eee71deea1787b1b19
yunyoung1819 <yunyoung1819@hanmail.net>
2021-06-08 06:23:17 +0900

    # 가입 확인 이메일 전송 기능 구현

---
+++ src/main/java/com/studysomoim/main/MainController.java
@@ -0,0 +1,20 @@
+package com.studysomoim.main;
+
+import com.studysomoim.account.CurrentUser;
+import com.studysomoim.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String home(@CurrentUser Account account, Model model) {
+        if (account != null) {
+            model.addAttribute(account);
+        }
+
+        return "index";
+    }
+}
